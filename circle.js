cded0ca5b8a9ab5bb0c5c07005525c3359acaafb
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2024-01-31 22:49:53 +0000

    Update code style because why not

--- circle.js
+++ circle.js
@@ -4,18 +4,18 @@ const { PI } = Math;
 
 /**
  * Calculates the area of a circle.
- * @param {number} radius The radius of the circle.
+ * @param {number} r The radius of the circle.
  * @returns {number} The area of the circle.
  */
-exports.area = function (radius) {
-	return PI * radius ** 2;
+exports.area = function (r) {
+	return PI * r ** 2;
 };
 
 /**
  * Calculates the circumference of a circle.
- * @param {number} radius The radius of the circle.
+ * @param {number} r The radius of the circle.
  * @returns {number} The circumference of the circle.
  */
-exports.circumference = function (radius) {
-	return 2 * PI * radius;
+exports.circumference = function (r) {
+	return 2 * PI * r;
 };
--- circle.test.js
+++ circle.test.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const { expect, it } = require("@jest/globals");
-const circle = require("./circle");
+var { expect, it } = require("@jest/globals");
+var circle = require("./circle");
 
-it("Calculates the area of a circle", function () {
-	const area = circle.area(37);
+it("Calculates the area of a circle", () => {
+	var area = circle.area(37);
 	expect(area).toBeCloseTo(4300.84);
 });
 
-it("Calculates the circumference of a circle", function () {
-	const circumference = circle.circumference(52);
+it("Calculates the circumference of a circle", () => {
+	var circumference = circle.circumference(52);
 	expect(circumference).toBeCloseTo(326.73);
 });
